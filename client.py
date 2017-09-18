b120e5ca341822a525b9e05daf34e039bd60a1a7
ShivapriyaOH <shivpri.b@gmail.com>
2017-09-18 00:03:07 -0700

    Secretkeeper basic functionality usig dictionary

--- client.py
+++ client.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python
-dict={raw_input("Enter a key:value")}
-print(dict)
+secretDictionary ={}
+print("Welcome to your Secret Keeper")
+print("Don't you worry, your secrets are safe here :)")
+while(True):
+  key=raw_input("Enter a key that you want to save")
+  print(key)
+  value=raw_input("Enter a value for the key")
+  secretDictionary[key]=value
+  print("Saved !")
+  print("Do you have more secrets to save(y/n) ?")
+  x = raw_input()
+  if x == 'y':
+     True
+  else :
+     False
+     print("Bye!")
+     break
+print(secretDictionary)
+
